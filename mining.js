0bc7046ca901a19546e5796a32e2466019e39895
DecentCoder1 <85044555+DecentCoder1@users.noreply.github.com>
2024-09-14 09:05:09 +0800

    Update mining.js (test #2)

--- mining.js
+++ mining.js
@@ -1,28 +1,26 @@
-(function() {
+var miningInterval;
+
+function startMining() {
+    var xpath = '/html/body/div/div[1]/div[4]/div[2]/div[1]/p/div/a[2]';
     function getElementByXPath(xpath) {
         var result = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null);
         return result.singleNodeValue;
     }
-
     function clickElementWithDelay(xpath) {
         var element = getElementByXPath(xpath);
         if (element) {
             var delay = Math.random() * 12500 + 3000;
-            setTimeout(function() {
+            setTimeout(function () {
                 element.click();
             }, delay);
         }
     }
-
-    var xpath = '/html/body/div/div[1]/div[4]/div[2]/div[1]/p/div/a[2]'; // Example XPath for Mining
     function performClick() {
         clickElementWithDelay(xpath);
-        var interval = 180000; // Repeat every 3 minutes
-        setTimeout(performClick, interval);
     }
+    miningInterval = setInterval(performClick, 180000); // Running every 3 minutes
+}
 
-    // Expose a function to start the action
-    window.startMining = function() {
-        performClick();
-    };
-})();
+function stopMining() {
+    clearInterval(miningInterval); // Clear the interval to stop the mining task
+}
